51ae46e6b0b15932f067a949e9f933df47f16ab5
Luca Ghielmetti <luca.ghielmetti@dos-group.com>
2019-05-17 10:21:45 +0200

    Initial setup

---
+++ src/main/java/it/lukegh/cachetest/CacheTestApplication.java
@@ -0,0 +1,13 @@
+package it.lukegh.cachetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheTestApplication.class, args);
+	}
+
+}
